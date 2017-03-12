c7774f211aa6950a5edc13b9959533cac97480fb
jyoerye <iyer_jyothsna@yahoo.com>
2017-03-12 19:45:35 -0400

    Changing localhost to ec2instance of slave

--- angular-client/src/app/dbcrud.service.ts
+++ angular-client/src/app/dbcrud.service.ts
@@ -16,7 +16,7 @@ export class DbcrudService {
 	  let headers = new Headers({'Content-Type':'application/json'});
 	  
 	  console.log("In service "+lname)
-	  return this.http.post('http://localhost:8081/people/',JSON.stringify({firstName : fname, lastName : lname }),{headers:headers})
+	  return this.http.post('http://ec2-35-165-52-134.us-west-2.compute.amazonaws.com:8081/people/',JSON.stringify({firstName : fname, lastName : lname }),{headers:headers})
 	  .map(response=>response.json())
 	  .catch(error=>Observable.throw(error.statusText));
 	  
